98244f8f7249dc94d13842c59c8faa6c677cb388
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2023-06-24 23:38:00 +0530

    added favicon and text-font

--- src/components/Home/TitleMessage/Message.jsx
+++ src/components/Home/TitleMessage/Message.jsx
@@ -4,12 +4,14 @@ import "./message.css";
 const Message = () => {
   return (
     <div className="text-yellow flex flex-col items-center">
-      <div>
-        <span className="text-white text-6xl">Code</span>
-        <span className="text-6xl">Tantra</span>
+      <div className="codetantra">
+        <p>
+          <span className="text-white text-6xl">Code</span>
+          <span className="text-6xl">Tantra</span>
+        </p>
       </div>
       <br></br>
-      <p> PRESENTS </p>
+      <p className="mt-1"> PRESENTS </p>
       <img id="logo" width={500} height={500} src={logo}></img>
     </div>
   );
