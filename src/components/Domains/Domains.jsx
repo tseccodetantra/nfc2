7cf40f4953a158a44c340f9961f3d02cd0526808
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2023-06-25 12:08:10 +0530

    domains done

--- src/components/Domains/Domains.jsx
+++ src/components/Domains/Domains.jsx
@@ -7,26 +7,29 @@ import "./domains.css";
 const Domains = () => {
   return (
     <div className="text-yellow mt-[10rem] bg-nGray rounded-[5rem]">
-      <div className="flex items-center justify-center text-6xl pt-[5rem]">
+      <div
+        id="domain-head"
+        className="flex items-center justify-center text-6xl pt-[5rem]"
+      >
         DOMAINS
       </div>
       <div className="flex flex-col items-center justify-center text-2xl">
         <div id="up" className="flex">
-          <div className="flex flex-col justify-center items-center p-[5rem]">
+          <div className="flex flex-col justify-center items-center p-[5rem] domain-container">
             <img src={web} width={200} height={200} className="invert"></img>
             <div className="mt-5">Web/App Development</div>
           </div>
-          <div className="flex flex-col justify-center items-center p-[5rem]">
+          <div className="flex flex-col justify-center items-center p-[5rem] domain-container">
             <img src={ai} width={200} height={200} className="invert"></img>
             <div className="mt-5">AI / ML</div>
           </div>
         </div>
         <div id="down" className="flex">
-          <div className="flex flex-col justify-center items-center p-[5rem]">
+          <div className="flex flex-col justify-center items-center p-[5rem] domain-container">
             <img src={bc} width={200} height={200} className="invert"></img>
             <div className="mt-5">Blockchain</div>
           </div>
-          <div className="flex flex-col justify-center items-center p-[5rem]">
+          <div className="flex flex-col justify-center items-center p-[5rem] domain-container">
             <img src={social} width={200} height={200} className="invert"></img>
             <div className="mt-5">Social</div>
           </div>
